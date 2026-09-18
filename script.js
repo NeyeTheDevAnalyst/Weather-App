9fe1443cdccea462de4f43f75282830692c76072
Matilda Eyubeh <thamatildaa@gmail.com>
2026-09-18 03:40:38 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,77 @@
+// http://api.weatherapi.com/v1/current.json?key=b4be675324244a0293a112747260404&q=Lagos&aqi=no
+
+const temperatureField = document.querySelector(".temp");
+const locationField = document.querySelector(".time_location p");
+const dateandTimeField = document.querySelector(".time_location span");
+const conditionField = document.querySelector(".condition_value"); 
+const searchField = document.querySelector(".search_area");
+const form= document.querySelector('form');
+
+form.addEventListener('submit', searchLocation);
+
+let target = 'Lagos'
+
+const fetchResults = async (targetLocation) =>{
+         let url = `https://api.weatherapi.com/v1/current.json?key=b4be675324244a0293a112747260404&q=${targetLocation}&aqi=no`
+
+         const res = await fetch(url);
+
+         const data = await res.json();
+
+         console.log(data);
+
+         let locationName = data.location.name
+
+         let time = data.location.localtime
+
+         let temp = data.current.temp_c
+
+         let condition = data.current.condition.text
+
+         updateUI(temp, locationName, time, condition);
+
+};
+
+function updateUI(temp, locationName, time, condition){
+
+        let splitDate = time.split(" ")[0];
+
+        let splitTime = time.split(" ")[1];
+
+        let currentDay = getDayName(new Date(splitDate).getDay());
+
+        temperatureField.innerText = temp + "°C";
+        locationField.innerText = locationName;
+        dateandTimeField.innerText = `${splitDate}, ${currentDay}, ${splitTime}`;
+        conditionField.innerText = condition;
+};
+
+function searchLocation(e){
+         e.preventDefault();
+
+         target = searchField.value;
+
+         fetchResults(target);
+};
+
+fetchResults(target);
+
+function getDayName(number){
+           switch(number){
+            case 0:
+                return "Sunday";
+            case 1:
+                return "Monday";
+            case 2:                
+                return "Tuesday";
+            case 3:
+                return "Wednesday";
+            case 4:                
+                return "Thursday";
+            case 5:                
+                return "Friday";
+            case 6:                
+                return "Saturday";
+
+           }
+};
